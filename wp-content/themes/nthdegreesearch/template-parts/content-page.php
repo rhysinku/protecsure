12bf8caf1a6fd51678b94316f31570e728567971
rhysinku <rhysinvillahermosa16@gmail.com>
2025-11-02 10:43:24 +0800

    update content media template

--- wp-content/themes/nthdegreesearch/template-parts/content-page.php
+++ wp-content/themes/nthdegreesearch/template-parts/content-page.php
@@ -2,6 +2,23 @@
 
     <div class="entry-content">
         <?php the_content(); ?>
+
+        <div class="test relative">
+            <div class="container container-sm">
+                <div>
+                    <div class="">
+                        <figure class="nds-figure-media lg:absolute top-0 bottom-0 left-0 max-w-[707px] w-full h-full">
+                            <img class="absolute top-0 w-full h-full" src="https://picsum.photos/707/426" alt="" decoding="async" loading="lazy" width="707" height="426">
+                        </figure>
+                    </div>
+                    <div class="max-w-[464px] w-full lg:ml-auto">
+                        <span>WHY US</span>
+                        <h2>We Go Where Business Goes</h2>
+                        <p>For over 30 years, we’ve specialised in creating simple, niche insurance products trusted by businesses on the go. Backed by local expertise and a fast, easy claims service, we make insurance straightforward, so your clients can focus on what matters most: getting on with business.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 
 </article>
