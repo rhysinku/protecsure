83b2770c098fa45b8b203de0905d0e087d565d58
rhysinku <rhysinvillahermosa16@gmail.com>
2025-11-02 11:05:20 +0800

    content media static

--- wp-content/themes/nthdegreesearch/template-parts/content-page.php
+++ wp-content/themes/nthdegreesearch/template-parts/content-page.php
@@ -3,15 +3,32 @@
     <div class="entry-content">
         <?php the_content(); ?>
 
-        <div class="test relative">
+        <div class="prt-content-media relative">
             <div class="container container-sm">
-                <div>
+                <div class="flex flex-col items-center">
                     <div class="">
-                        <figure class="nds-figure-media lg:absolute top-0 bottom-0 left-0 max-w-[707px] w-full h-full">
-                            <img class="absolute top-0 w-full h-full" src="https://picsum.photos/707/426" alt="" decoding="async" loading="lazy" width="707" height="426">
+                        <figure class="nds-figure-media lg:absolute top-0 bottom-0 left-0 max-w-[707px] lg:w-2/4 w-full h-full">
+                            <img class="lg:absolute top-0 w-full h-full object-cover!" src="https://protecsure.ddev.site/wp-content/uploads/2025/10/image.png" alt="" decoding="async" loading="lazy" width="707" height="426">
                         </figure>
                     </div>
-                    <div class="max-w-[464px] w-full lg:ml-auto">
+                    <div class="lg:max-w-[464px] w-full lg:ml-auto py-16">
+                        <span>WHY US</span>
+                        <h2>We Go Where Business Goes</h2>
+                        <p>For over 30 years, we’ve specialised in creating simple, niche insurance products trusted by businesses on the go. Backed by local expertise and a fast, easy claims service, we make insurance straightforward, so your clients can focus on what matters most: getting on with business.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="prt-content-media relative">
+            <div class="container container-sm">
+                <div class="flex flex-col items-center">
+                    <div class="">
+                        <figure class="nds-figure-media lg:absolute top-0 bottom-0 right-0 max-w-[707px] lg:w-2/4 w-full h-full">
+                            <img class="lg:absolute top-0 w-full h-full object-cover!" src="https://protecsure.ddev.site/wp-content/uploads/2025/10/image.png" alt="" decoding="async" loading="lazy" width="707" height="426">
+                        </figure>
+                    </div>
+                    <div class="lg:max-w-[464px] w-full lg:mr-auto py-16">
                         <span>WHY US</span>
                         <h2>We Go Where Business Goes</h2>
                         <p>For over 30 years, we’ve specialised in creating simple, niche insurance products trusted by businesses on the go. Backed by local expertise and a fast, easy claims service, we make insurance straightforward, so your clients can focus on what matters most: getting on with business.</p>
